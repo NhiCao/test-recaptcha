2b445f5a0cabc3d11a8b4b8ebedcd3a94af5685a
Cao Hai Nhi <nhicao1995@gmail.com>
2025-01-02 06:23:50 +0700

    update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import sys
 
 from flask import Flask, render_template, request, jsonify
 
@@ -22,35 +23,34 @@ def hello_world():
 
 @app.route('/verify-recaptcha', methods=['POST'])
 def verify_recaptcha():
-    return "hi"
-    # recaptcha_response = request.form.get('recaptchaResponse')
-    # print('=================recaptcha_response===================')
-    # print(recaptcha_response)
-    # print('====================================')
+    recaptcha_response = request.form.get('recaptchaResponse')
+    print('=================recaptcha_response===================')
+    print(recaptcha_response, file=sys.stderr)
+    print('====================================')
 
-    # if not recaptcha_response:
-    #     return jsonify({'success': False, 'message': 'Missing reCAPTCHA response.'}), 400
+    if not recaptcha_response:
+        return jsonify({'success': False, 'message': 'Missing reCAPTCHA response.'}), 400
 
-    # secret_key = os.getenv('RECAPTCHA_SECRET')
-    # if not secret_key:
-    #     return jsonify({'success': False, 'message': 'Secret key is not configured.'}), 500
+    secret_key = os.getenv('RECAPTCHA_SECRET')
+    if not secret_key:
+        return jsonify({'success': False, 'message': 'Secret key is not configured.'}), 500
 
-    # google_verify_url = 'https://www.google.com/recaptcha/api/siteverify'
-    # payload = {
-    #     'secret': secret_key,
-    #     'response': recaptcha_response
-    # }
-    # response = requests.post(google_verify_url, data=payload)
-    # verification = response.json()
+    google_verify_url = 'https://www.google.com/recaptcha/api/siteverify'
+    payload = {
+        'secret': secret_key,
+        'response': recaptcha_response
+    }
+    response = requests.post(google_verify_url, data=payload)
+    verification = response.json()
 
-    # if verification.get('success'):
-    #     return jsonify({'success': True, 'message': 'Verification successful.'})
-    # else:
-    #     return jsonify({
-    #         'success': False,
-    #         'message': 'Verification failed.',
-    #         'error_codes': verification.get('error-codes', [])
-    #     }), 400
+    if verification.get('success'):
+        return jsonify({'success': True, 'message': 'Verification successful.'})
+    else:
+        return jsonify({
+            'success': False,
+            'message': 'Verification failed.',
+            'error_codes': verification.get('error-codes', [])
+        }), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

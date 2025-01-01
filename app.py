2ef380e7c6e8bf0ee75fab5cdf93f3b5bd1f224c
Cao Hai Nhi <nhicao1995@gmail.com>
2025-01-01 21:23:03 +0700

    update

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
     return render_template('index.html')
 
-@app.route("/verify-recaptcha")
+@app.route('/verify-recaptcha', methods=['POST'])
 def verify_recaptcha():
+    data = request.json
+    return jsonify(data)
     
-    return "<p>Hello, World!</p>"
+    # return '<p>Hello, World!</p>'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

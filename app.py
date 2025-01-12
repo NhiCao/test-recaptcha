1084076ceaa189b3983de1392095309e5506dd8c
Cao Hai Nhi <nhicao1995@gmail.com>
2025-01-12 15:10:50 +0700

    update

--- app.py
+++ app.py
@@ -24,41 +24,48 @@ def hello_world():
 
 @app.route('/verify-recaptcha', methods=['POST'])
 def verify_recaptcha():
+    arguments = request.args
+    print('*=================arguments===================*', file=sys.stderr)
+    print(json.dumps(arguments, sort_keys=True, indent=4))
+    print('*====================================*', file=sys.stderr)
     recaptcha_response = request.form.get('recaptchaResponse')
-    print('=================recaptcha_response===================', file=sys.stderr)
+    print('*=================recaptcha_response===================*******', file=sys.stderr)
     print(recaptcha_response, file=sys.stderr)
-    print('====================================', file=sys.stderr)
-
-    if not recaptcha_response:
-        return jsonify({'success': False, 'message': 'Missing reCAPTCHA response.'}), 400
-
-    secret_key = os.getenv('RECAPTCHA_SECRET')
-    if not secret_key:
-        return jsonify({'success': False, 'message': 'Secret key is not configured.'}), 500
-
-    google_verify_url = 'https://www.google.com/recaptcha/api/siteverify'
-    payload = {
-        'secret': secret_key,
-        'response': recaptcha_response
-    }
-    print('=================payload===================', file=sys.stderr)
-    print(payload, file=sys.stderr)
-    response = requests.post(google_verify_url, data=payload)
-    verification = response.json()
+    print('*====================================*', file=sys.stderr)
     
-    if verification.get('success'):
-        print('=================success===================', file=sys.stderr)
-        print('====================================', file=sys.stderr)
-        return jsonify({'success': True, 'message': 'Verification successful.'})
-    else:
-        print('=================failure===================', file=sys.stderr)
-        print(json.dumps(verification, indent=2), file=sys.stderr)
-        print('====================================', file=sys.stderr)
-        return jsonify({
-            'success': False,
-            'message': 'Verification failed.',
-            'error_codes': verification.get('error-codes', [])
-        }), 400
+    return "hello"
+
+    # if not recaptcha_response:
+    #     # return jsonify({'success': False, 'message': 'Missing reCAPTCHA response.'}), 400
+    #     return "nhi"
+
+    # secret_key = os.getenv('RECAPTCHA_SECRET')
+    # if not secret_key:
+    #     return jsonify({'success': False, 'message': 'Secret key is not configured.'}), 500
+
+    # google_verify_url = 'https://www.google.com/recaptcha/api/siteverify'
+    # payload = {
+    #     'secret': secret_key,
+    #     'response': recaptcha_response
+    # }
+    # print('=================payload===================', file=sys.stderr)
+    # print(payload, file=sys.stderr)
+    # response = requests.post(google_verify_url, data=payload)
+    # verification = response.json()
+    
+    # if verification.get('success'):
+    #     print('=================success===================', file=sys.stderr)
+    #     print('====================================', file=sys.stderr)
+    #     return jsonify({'success': True, 'message': 'Verification successful.'})
+    # else:
+    #     print('=================failure===================', file=sys.stderr)
+    #     print(json.dumps(verification, indent=2), file=sys.stderr)
+    #     print('====================================', file=sys.stderr)
+    #     return jsonify({
+    #         'success': False,
+    #         'message': 'Verification failed.',
+    #         'error_codes': verification.get('error-codes', [])
+    #     }), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
